2d1533e86ac4ab5f34fc419732e5d51dbfebd224
Larys Brianna <lbrianna@oa-i6.msk.21-school.ru>
2022-03-25 14:11:25 +0300

    try

---
+++ ft_printf/utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrianna <lbrianna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 14:10:10 by lbrianna          #+#    #+#             */
+/*   Updated: 2022/03/25 14:10:27 by lbrianna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[i])
+		i++;
+	write(1, str, i);
+	return (i);
+}
